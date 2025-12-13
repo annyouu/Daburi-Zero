f75102f2a57c1ffb395bd3f2e40b5901bc89b78a
antou <andong041205@gmail.com>
2025-12-13 23:38:47 +0900

    [add] ログイン処理実装

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module destinyface
+
+go 1.24.0
---
+++ backend/internal/domain/repository/user_repo.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"context"
+	"destinyface/internal/domain/entity"
+)
+
+// ユーザーデータの永続化を抽象化する
+// Create (新規登録、新しいユーザー情報をDBに挿入する)
+type UserRepository interface {
+	Create(ctx context.Context, user *entity.User) error
+}
