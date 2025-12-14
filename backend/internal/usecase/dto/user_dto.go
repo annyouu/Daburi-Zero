f1936c9196c5ab5e4862a1552975f0ab3040b1f8
antou <andong041205@gmail.com>
2025-12-14 18:53:53 +0900

    [add] usecase 新規登録作成完了

--- backend/internal/usecase/dto/user_dto.go
+++ backend/internal/usecase/dto/user_dto.go
@@ -1,12 +1,14 @@
 package dto
 
+import "time"
+
 // -- 入力データ --
 
 // 新規登録用、そのままのパスワードを受け取る
 type UserRegisterInput struct {
-	Name     string `json:"name"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" validate:"required"`
+    Email    string `json:"email" validate:"required,email"` 
+    Password string `json:"password" validate:"required,min=8"`
 }
 
 // ログイン用
@@ -15,12 +17,13 @@ type UserLoginInput struct {
 	Password string `json:"password"`
 }
 
+// 顔写真登録用
+
 // プロフィール更新用、更新したい項目のみ受け取る
 type UserUpdateInput struct {
 	Name string  `json:"name"`
 	ProfileImageURL string `json:"profile_image_url"`
 }
-
 
 // -- 出力データ --
 
@@ -29,7 +32,7 @@ type UserOutput struct {
 	ID string  `json:"id"`
 	Name string  `json:"name"`
 	Email string  `json:"email"`
-	ProfileImageURL string  `json:"profile_image_url"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 // ログイン成功時にトークンを返す用
--- backend/internal/usecase/user_usecase.go
+++ backend/internal/usecase/user_usecase.go
@@ -11,6 +11,7 @@ import (
 	"destinyface/internal/domain/repository"
 	"destinyface/internal/usecase/dto"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -28,35 +29,66 @@ type UserUseCaseInterface interface {
 
 type UserUseCase struct {
 	userRepo repository.UserRepositoryInterface
+	validator *validator.Validate
 }
 
 // コンストラクタ
-// func NewUserUseCase(userRepo repository.UserRepositoryInterface) UserUseCaseInterface {
-// 	return &UserUseCase{
-// 		userRepo: userRepo
-// 	}
-// }
+func NewUserUseCase(userRepo repository.UserRepositoryInterface) UserUseCaseInterface {
+	return &UserUseCase{
+		userRepo: userRepo,
+		validator: validator.New(),
+	}
+}
 
 // 新規登録
 func (u *UserUseCase) Register(ctx context.Context, input *dto.UserRegisterInput) (*dto.UserOutput, error) {
 
-	// いずれかの入力値が空かどうか
-	if input.Name == "" || input.Email == "" || input.Password == "" {
+	// DTOタグに定義されたルールでバリデーションを実行
+	if err := u.validator.Struct(input); err != nil {
 		return nil, domain.ErrInvalidInput
 	}
 
-	// Email形式の正規化チェック(詳細なバリデーション)
+	// Emailの重複チェック
+	_, err := u.userRepo.FindByEmail(ctx, input.Email)
 
-	// 入力値がすでにあるか重複しているかどうか
+	// nilだったら、ユーザーが重複しているということ
+	if err == nil {
+		return nil, domain.ErrEmailAlreadyExists
+	}
 
-	// OKだったら、パスワードのハッシュかを行う
+	if !errors.Is(err, domain.ErrNotFound) {
+		return nil, fmt.Errorf("FindByEmailの処理に失敗しました: %w", err)
+	}
 
+	// パスワードのハッシュ化を行う
+	hashedPassword, hashErr := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if hashErr != nil {
+		return nil, fmt.Errorf("パスワードのハッシュ化に失敗しました： %w", err)
+	}
 
 	// エンティティの生成
+	now := time.Now()
+	newUser := &entity.User{
+		ID:           uuid.New().String(),
+        Email:        input.Email,
+        PasswordHash: string(hashedPassword),
+        Name:         input.Name,
+        CreatedAt:    now,
+        UpdatedAt:    now,
+	}
 
 	// リポジトリを呼び出し、DBへ保存する
+	if createdErr := u.userRepo.Create(ctx, newUser); createdErr != nil {
+		return nil, fmt.Errorf("DBへの保存に失敗しました: %w", createdErr)
+	}
 
 	// 成功レスポンスを返す
+	return &dto.UserOutput{
+		ID:    newUser.ID,
+        Name:  newUser.Name,
+        Email: newUser.Email,
+		CreatedAt: newUser.CreatedAt,
+	}, nil
 }
 
 // ログイン
