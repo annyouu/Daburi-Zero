5e1eb4bda2a72c08225b9531211b31c025e950ca
antou <andong041205@gmail.com>
2026-01-05 07:07:52 +0900

    update: status追加でより堅牢に

--- backend/internal/usecase/dto/user_dto.go
+++ backend/internal/usecase/dto/user_dto.go
@@ -4,19 +4,27 @@ import "time"
 
 // -- 入力データ --
 
-// 新規登録用、そのままのパスワードを受け取る
+// 1: 新規登録（PENDING_NAMEへ）
 type UserRegisterInput struct {
-    Email    string `json:"email" validate:"required,email"` 
-    Password string `json:"password" validate:"required,min=8"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+// 2: 名前設定（PENDING_IMAGEへ）
+type UserSetupNameInput struct {
+	Name string `json:"name" validate:"required"`
+}
+
+// 3: 画像設定（ACTIVEへ）
+type UserSetupImageInput struct {
+	ProfileImageURL string `json:"profile_image_url" validate:"required"`
 }
 
 // ログイン用
 type UserLoginInput struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
 }
-
-// 顔写真登録用
 
 // プロフィール更新用、更新したい項目のみ受け取る
 type UserUpdateInput struct {
@@ -32,6 +40,7 @@ type UserOutput struct {
 	Name string  `json:"name"`
 	Email string  `json:"email"`
 	ProfileImageURL string `json:"profile_image_url"`
+	Status string    `json:"status"`
 	Token string    `json:"token,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 }
