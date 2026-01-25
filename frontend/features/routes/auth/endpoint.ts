e408e13097006ac4669e541a51f517c00901b855
antou <andong041205@gmail.com>
2026-01-25 12:44:33 +0900

    add: authのendpoints実装完了

--- frontend/features/routes/auth/endpoint.ts
+++ frontend/features/routes/auth/endpoint.ts
@@ -1,17 +1,25 @@
+// 認証に関するapiのみを叩く役割
 import { apiClient } from "@/lib/apiClient";
+import { UserResponse, AuthTokenOutput } from "@/type/user";
 
-// UserLoginInput
-export interface SignupRequest {
-    email: string;
-    password: string;
-}
+export const authEndpoints = {
+    /**
+     * 新規登録
+     * @param data { email, password }
+     * @returns UserResponse (status: PENDING_NAME などが返る)
+     */
 
-// AuthTokenOutput
-export interface SignupResponse {
-    message: string;
-    token: string;
-}
+    register: (data: any): Promise<UserResponse> => {
+        return apiClient.post<UserResponse>("/auth/register", data);
+    },
 
-export const signupRequest = async (data: SignupRequest) => {
-    return await apiClient.post("/api/v1/signup", data);
-}
+    /**
+     * ログイン
+     * @param data { email, password }
+     * @returns AuthTokenOutput (token と status が返る)
+     */
+
+    login: (data: any): Promise<AuthTokenOutput> => {
+        return apiClient.post<AuthTokenOutput>("/auth/login", data);
+    },
+};
