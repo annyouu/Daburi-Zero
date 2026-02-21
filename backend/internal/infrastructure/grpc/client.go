7c97a4968d2ff50b7a278e6f03328e75d2f9b3c2
antou <andong041205@gmail.com>
2026-02-21 13:50:08 +0900

    update: client.go修正

--- backend/internal/infrastructure/grpc/client.go
+++ backend/internal/infrastructure/grpc/client.go
@@ -2,8 +2,6 @@ package grpc
 
 import (
 	"context"
-	"log"
-	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -11,33 +9,63 @@ import (
 	pb "destinyface/gen/proto"
 )
 
-// PythonのMLサーバーに画像を投げて解析結果をもらうメソッド
-func AnalyzeImageWithPython(imageData []byte) (*pb.AnalyzeResponse, error) {
-	// Pythonサーバーへの接続設定
-	conn, err := grpc.Dial("python-ml:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+type ImageAnalyzerClient struct {
+	client pb.ImageAnalyzerClient
+	conn *grpc.ClientConn
+}
+
+// 接続を保持したクライアントを作成する
+func NewImageAnalyzerClient(target string) (*ImageAnalyzerClient, error) {
+	conn, err := grpc.Dial(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
 
-	defer conn.Close()
+	return &ImageAnalyzerClient{
+		client: pb.NewImageAnalyzerClient(conn),
+		conn: conn,
+	}, nil
+}
 
-	client := pb.NewImageAnalyzerClient(conn)
+func (c *ImageAnalyzerClient) Close() error {
+	return c.conn.Close()
+}
 
-	// タイムアウト付きのコンテキストを作成
-	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-	defer cancel()
-
-	// リクエストを送信
+func (c *ImageAnalyzerClient) AnalyzeImage(ctx context.Context, imageData []byte) (*pb.AnalyzeResponse, error) {
 	req := &pb.AnalyzeRequest{
 		ImageData: imageData,
 	}
 
-	log.Println("Python MLサーバーにリクエストを送信中...")
-	res, err := client.AnalyzeImage(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-
-	return res, nil
-
+	return c.client.AnalyzeImage(ctx, req)
 }
+
+// // PythonのMLサーバーに画像を投げて解析結果をもらうメソッド
+// func AnalyzeImageWithPython(imageData []byte) (*pb.AnalyzeResponse, error) {
+// 	// Pythonサーバーへの接続設定
+// 	conn, err := grpc.Dial("python-ml:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	defer conn.Close()
+
+// 	client := pb.NewImageAnalyzerClient(conn)
+
+// 	// タイムアウト付きのコンテキストを作成
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+// 	defer cancel()
+
+// 	// リクエストを送信
+// 	req := &pb.AnalyzeRequest{
+// 		ImageData: imageData,
+// 	}
+
+// 	log.Println("Python MLサーバーにリクエストを送信中...")
+// 	res, err := client.AnalyzeImage(ctx, req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return res, nil
+
+// }
